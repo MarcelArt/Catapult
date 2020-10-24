6b62f74df0685e9502ce1a01cc08d9294453985a
MarcelArt <marcel.aritonang@gmail.com>
2020-10-24 21:18:00 +0700

    first commit to remote
    Player can slingshot the red ball by holding and dragging the mouse, the more you move the more force it applies to the ball, also has object placing mechanic change the state of the player object from build to play or vice versa

---
+++ Assets/BuildMode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMode : MonoBehaviour
+{
+    public GameObject catapult;
+    public Player player;
+
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+
+        if(Input.GetMouseButtonDown(0) && player.state == GameState.BUILD) {
+            Instantiate(catapult, mousePos, transform.rotation);
+        }
+
+        if(player.state == GameState.PLAY) {
+            sprite.enabled = false;
+        }
+        else {
+            sprite.enabled = true;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState {
+    BUILD,
+    PLAY
+}
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    public float force;
+    public GameState state = GameState.BUILD;
+
+    public Vector2 mouseStart;
+    public Vector2 mousePos;
+    public Vector2 mouseEnd;
+
+    private Vector2 forceDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // rb.AddForce(new Vector2(force, force), ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == GameState.PLAY) {
+            if(Input.GetMouseButtonDown(0)) {
+                mouseStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else if(Input.GetMouseButton(0)) {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else if(Input.GetMouseButtonUp(0)) {
+                mouseEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                forceDir = (mouseEnd - mouseStart);
+                // Debug.Log(forceDir);
+                rb.AddForce(-forceDir, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Enemy") {
+            foreach(ContactPoint2D point2D in other.contacts) {
+                Debug.DrawLine(point2D.point, point2D.point + point2D.normal, Color.red, 10f);
+                if(point2D.normal.y > .5f) {
+                    Destroy(other.gameObject);
+                }
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Catapult") {
+            Debug.Log("Start catapulting or something");
+        }
+    }
+}
